ac380a37f2b932a46bd4e62c0e46adba974a54c3
Yuu NAKAJIMA <yuu.nakajima@is.sci.toho-u.ac.jp>
2018-12-09 22:18:51 +0900

    pom.xmlの修正

--- src/test/java/org/magcruise/gaming/examples/minority/MinorityGameLauncherTest.java
+++ src/test/java/org/magcruise/gaming/examples/minority/MinorityGameLauncherTest.java
@@ -2,13 +2,11 @@ package org.magcruise.gaming.examples.minority;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.nkjmlab.util.db.H2Server;
 
 public class MinorityGameLauncherTest {
 
 	@Before
 	public void setUp() throws Exception {
-		H2Server.start();
 	}
 
 	@Test
